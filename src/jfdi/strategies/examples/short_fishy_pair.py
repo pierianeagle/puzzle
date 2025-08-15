c228211be7c70b1dc501610ab80ff4e163f9aecc
miko <spring_special_0f@icloud.com>
2025-08-15 21:01:33 +0100

    Update short fishy pair strategy

--- src/jfdi/strategies/examples/short_fishy_pair.py
+++ src/jfdi/strategies/examples/short_fishy_pair.py
@@ -1,33 +1,43 @@
 from typing import Literal
 
 import pandas as pd
+from nautilus_trader.common.enums import LogColor
 from nautilus_trader.model import (
     Bar,
     BarSpecification,
     BarType,
+    Currency,
     InstrumentId,
+    Venue,
 )
 from nautilus_trader.model.enums import AggregationSource
-from nautilus_trader.trading.strategy import StrategyConfig
 
-from jfdi.extensions.strategies.weight import WeightStrategy
+from jfdi.actors.equity import get_all_unrealised_pnls, get_equities, get_equity
+from jfdi.extensions.strategies.weight import WeightStrategy, WeightStrategyConfig
 
 
-class ShortFishyPairStrategyConfig(StrategyConfig):
+class ShortFishyPairStrategyConfig(WeightStrategyConfig):
     instrument_ids: dict[Literal["long", "short"], InstrumentId]
     bar_spec: BarSpecification
     weight: float  # %
     bias: float  # %
     threshold: float  # %
+    account_venue: Venue
+    exchange_rate_venue: Venue
+    reporting_currency: Currency
     order_id_tag: str  # 000
 
 
 class ShortFishyPairStrategy(WeightStrategy):
     def __init__(self, config: ShortFishyPairStrategyConfig) -> None:
-        """A strategy that ranks sectors by their momentum."""
+        """A strategy that ranks sectors by their momentum.
+
+        This strategy assumes that the pair trades at the same venue, and keeps account
+        and exchange rate venues separate to support Interactive Brokers.
+        """
         super().__init__(config)
 
-        self.venue = self.config.instrument_ids["long"].venue
+        self.instrument_venue = self.config.instrument_ids["long"].venue
 
         self.bar_types = {
             k: BarType(v, self.config.bar_spec, AggregationSource.EXTERNAL)
@@ -42,7 +52,7 @@ class ShortFishyPairStrategy(WeightStrategy):
         self.previous_target_weights = {}
 
     def on_start(self) -> None:
-        self.account = self.portfolio.account(self.venue)
+        self.account = self.portfolio.account(self.config.account_venue)
 
         for bar_type in self.bar_types.values():
             self.subscribe_bars(bar_type)
@@ -60,17 +70,38 @@ class ShortFishyPairStrategy(WeightStrategy):
         self.last_timestamps[instrument_id] = bar.ts_event
 
         if len(set(self.last_timestamps.values())) == 1:
-            equities = self.get_equities()
+            balances_total = self.account.balances_total()
 
-            current_weights = self.get_current_weights(equities)
+            venues = {instrument.id.venue for instrument in self.cache.instruments()}
+
+            unrealised_pnls = get_all_unrealised_pnls(self, venues)
+
+            equities = get_equities(balances_total, unrealised_pnls)
+            equity = get_equity(
+                self,
+                equities,
+                self.config.exchange_rate_venue,
+                self.config.reporting_currency,
+            )
+
+            if equity is None:
+                self.log.warning("NO EXCHANGE RATES AVAILABLE.", LogColor.CYAN)
+                return
+
+            current_weights = self.get_current_weights(equity)
+
+            if any(value is None for value in current_weights.values()):
+                self.log.warning("NO EXCHANGE RATES AVAILABLE.", LogColor.CYAN)
+                return
+
             target_weights = self.get_target_weights()
             order_weights = self.get_order_weights(current_weights, target_weights)
 
             order_weights = {
-                k: v for k, v in order_weights.items() if abs(v) > self.config.threshold
+                k: v
+                for k, v in order_weights.items()
+                if abs(v) > self.config.weight * self.config.threshold
             }
-
-            equity = equities[self.account.base_currency]
 
             orders = self.create_orders(equity, order_weights)
 
@@ -85,13 +116,10 @@ class ShortFishyPairStrategy(WeightStrategy):
         """Calculate the portfolio's target weights (directional)."""
         target_weights = {
             instrument_id: -self.config.weight
-            * (1 / len(self.config.instrument_ids))
-            * float(
-                self.account.leverages().get(
-                    instrument_id, self.account.default_leverage
-                )
+            * (
+                (1 / len(self.config.instrument_ids))
+                + (1 if name == "long" else -1) * self.config.bias
             )
-            + (1 if name == "long" else -1) * self.config.bias
             for name, instrument_id in self.config.instrument_ids.items()
         }
 
