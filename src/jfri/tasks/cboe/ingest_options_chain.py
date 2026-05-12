6bb744369f89ce84db0f3ce4c1c770df16252114
aiko <spring_special_0f@icloud.com>
2026-05-12 21:29:01 +0100

    Create task that gets multiple unique symbols from a single ingested file

--- src/jfri/tasks/cboe/ingest_options_chain.py
+++ src/jfri/tasks/cboe/ingest_options_chain.py
@@ -1,13 +1,15 @@
 import json
 from pathlib import Path
 
+import pandas as pd
 from prefect import get_run_logger, task
 
+from jfri.contracts.occ import parse_occ_tickers
 from jfri.tasks.cboe.client import get_cboe_options_chain
 
 
 @task
-def ingest_todays_options_chain(ingest_path: Path, ticker: str):
+def ingest_todays_options_chain(ingested_path: Path, ticker: str):
     """Ingest today's EOD options chain as JSON."""
     logger = get_run_logger()
 
@@ -20,4 +22,17 @@ def ingest_todays_options_chain(ingest_path: Path, ticker: str):
         )
         return
 
-    ingest_path.write_text(json.dumps(data), encoding="utf-8")
+    ingested_path.write_text(json.dumps(data), encoding="utf-8")
+
+
+@task
+def get_unique_symbols(ingested_path: Path):
+    """Return the unique symbols EOD options chain as JSON."""
+    payload = json.loads(ingested_path.read_bytes())
+    records = payload["data"]["options"]
+
+    df_ingested = pd.DataFrame.from_records(records)
+
+    symbols = parse_occ_tickers(df_ingested["option"])["underlying"].unique()
+
+    return symbols
